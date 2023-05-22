446d59e24510f4c4bea61aa7fd3bfbde67509472
Ronaldo Arrais <arraisronaldo6@gmail.com>
2023-05-22 12:14:53 -0300

    Concluído

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 #Caso o usuário digite um valor de b² - 4ac menor do que 0, o programa deve imprimir A equação não possui raízes reais #e terminar.
 #Caso o usuário digite um valor de b² - 4ac igual a 0, o programa deve imprimir A equação possui uma raiz real #e terminar. Em seguida ele deve imprimir o valor da raiz com exatamente duas casas decimais, independentemente se o #número for inteiro ou não.
 #Caso o usuário digite um valor de b² - 4ac maior do que 0, o programa deve imprimir A equação possui duas raízes #reais e terminar. Em seguida ele deve imprimir o valor das raízes com exatamente duas casas decimais, #independentemente se o número for inteiro ou não. A primeira raiz deve ser menor do que a segunda raiz e termina
+
 Grau =  int(input("Digite um número;"))
 if Grau < 1:
     print ("Grau inválido")
@@ -27,31 +28,32 @@ if Grau == 1:
     if a != 0:
          b = int(input("Digite valor de b:"))
          x = - b/a
-         print(x)
+         print(f"{x:.2f}")
 if Grau == 2:
     print("A equação é do segundo grau")
     a2 = int(input("Digite o valor de a:"))
-    b2= int(input("Digite o valor de b:"))
-    c = int(input("Digite o valor de c:"))
-    Delta = (b2*b2 - 4 * a2 * c)
-    raiz1 = (- b2 + Delta**1/2)/2 * a2
-    raiz2 =(- b2 - Delta**0.5)/2 * a2
     if a2 == 0:
         print("Valor de a inválido")
-    if a2 != 0:   
-        if Delta < 0:
-            print("A equação não possui raízes reais")
-        if Delta == 0 :
-            print ("A equação possui uma raiz real")
-        
-            print(raiz1)
-        if Delta > 0:
-        
-            print("A equação possui duas raízes reais")
-            print(raiz2) 
-            print(raiz1)
-        
-        
+    else:
+        b2= int(input("Digite o valor de b:"))
+        c = int(input("Digite o valor de c:"))
+        Delta = ((b2**2) - (4 * a2 * c))
+        raiz1 = (- b2 + (Delta**0.5))/(2 * a2)
+        raiz2 = (- b2 - (Delta**0.5))/(2 * a2)
+        if a2 != 0:   
+            if Delta < 0:
+                print("A equação não possui raízes reais")
+            if Delta == 0 :
+                print ("A equação possui uma raiz real")
+            
+                print(f"{raiz1:.2f}")
+            if Delta > 0:
+            
+                print("A equação possui duas raízes reais")
+                print(f"{raiz2:.2f}") 
+                print(f"{raiz1:.2f}")
+            
+            
 
 
 
